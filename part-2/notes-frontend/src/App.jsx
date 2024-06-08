d6642b7be61f0b8112158ef8ee01e1614bb6f2b0
Jaimin Parmar <jaimins100@gmail.com>
2024-06-08 17:18:52 +0530

    create notes service

--- part-2/notes-frontend/src/App.jsx
+++ part-2/notes-frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import axios from 'axios'
 import { useState, useEffect } from 'react'
 import Note from "./components/Note"
+import noteService from './services/notes'
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
@@ -9,11 +9,11 @@ const App = (props) => {
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
         console.log('promise fulfilled')
-        setNotes(response.data)
+        setNotes(initialNotes)
       })
   }, [])
 
@@ -31,12 +31,12 @@ const App = (props) => {
       id: notes.length + 1,
     }
 
-    axios
-    .post('http://localhost:3001/notes', noteObject)
-    .then(response => {
-      setNotes(notes.concat(response.data))
-      setNewNote('')
-    })
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -49,8 +49,8 @@ const App = (props) => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
   
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(n => n.id !== id ? n : response.data))
+    noteService.update(id, changedNote).then(returnedNote => {
+      setNotes(notes.map(n => n.id !== id ? n : returnedNote))
     })
   }
 
