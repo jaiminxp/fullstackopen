1dfbe965df922ee45faee29f4f9bad8d9cb1daaf
Jaimin Parmar <jaimins100@gmail.com>
2024-06-08 18:45:28 +0530

    add styles and error message

--- part-2/notes-frontend/src/App.jsx
+++ part-2/notes-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import Note from "./components/Note"
 import noteService from './services/notes'
+import Notification from './components/Notification'
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     console.log('effect')
@@ -54,7 +56,12 @@ const App = (props) => {
         setNotes(notes.map((n) => (n.id !== id ? n : returnedNote)))
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`)
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter((n) => n.id !== id))
       })
   }
@@ -62,6 +69,7 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
