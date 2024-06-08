7ed0eaa801be44460f32226f0fa4ec9ce80eed23
Jaimin Parmar <jaimins100@gmail.com>
2024-06-08 20:48:55 +0530

    setup cors and logger middleware

--- part-3/notes-backend/index.js
+++ part-3/notes-backend/index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 let notes = [
   {
@@ -70,6 +83,12 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
