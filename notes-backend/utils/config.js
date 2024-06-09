b50a1a6e81cecad8d7e4b45be506b2a59ae47e87
Jaimin Parmar <jaimins100@gmail.com>
2024-06-09 11:49:13 +0530

    fix config

--- notes-backend/utils/config.js
+++ notes-backend/utils/config.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT
 const MONGO_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGO_URI
-    : process.env.MONGODB_URI
+    : process.env.MONGO_URI
 
 module.exports = {
   MONGO_URI,
