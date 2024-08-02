c4cda1ef6159c2cbb5501b76298f4bb03438cbcc
Jaimin Parmar <jaimins100@gmail.com>
2024-08-02 11:12:35 +0530

    isolate common test steps in beforeEach block

--- e2e-playwright/tests/note_app.spec.js
+++ e2e-playwright/tests/note_app.spec.js
@@ -1,9 +1,11 @@
 const { test, expect, describe } = require('@playwright/test')
 
 describe('Note app', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173')
+  })
 
+  test('front page can be opened', async ({ page }) => {
     const locator = page.getByText('Notes')
     await expect(locator).toBeVisible()
     await expect(
@@ -14,8 +16,6 @@ describe('Note app', () => {
   })
 
   test('login form can be opened', async ({ page }) => {
-    await page.goto('http://localhost:5173')
-
     await page.getByRole('button', { name: 'login' }).click()
     await page.getByTestId('username').fill('admin')
     await page.getByTestId('password').fill('admin123')
