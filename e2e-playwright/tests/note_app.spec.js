b2ebc7db251ad79cc7d31bf938a27f7d65075490
Jaimin Parmar <jaimins100@gmail.com>
2024-08-02 11:29:27 +0530

    add test for note creation

--- e2e-playwright/tests/note_app.spec.js
+++ e2e-playwright/tests/note_app.spec.js
@@ -1,4 +1,4 @@
-const { test, expect, describe } = require('@playwright/test')
+const { test, expect, describe, beforeEach } = require('@playwright/test')
 
 describe('Note app', () => {
   beforeEach(async ({ page }) => {
@@ -22,5 +22,22 @@ describe('Note app', () => {
     await page.getByRole('button', { name: 'login' }).click()
 
     await expect(page.getByText('Jaimin logged-in')).toBeVisible()
+  })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByTestId('username').fill('admin')
+      await page.getByTestId('password').fill('admin123')
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new note' }).click()
+      await page.getByRole('textbox').fill('a note created by playwright')
+      await page.getByRole('button', { name: 'save' }).click()
+
+      await expect(page.getByText('a note created by playwright')).toBeVisible()
+    })
   })
 })
