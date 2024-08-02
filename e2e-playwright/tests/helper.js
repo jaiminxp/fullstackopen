03e08cf215f6d93db40d53f7262f3302386a5b17
Jaimin Parmar <jaimins100@gmail.com>
2024-08-02 15:26:55 +0530

    rewrite note importance test
    - wait for note to get rendered
    - pause debugger before each test
    - use XPath in locator to get parent element

--- e2e-playwright/tests/helper.js
+++ e2e-playwright/tests/helper.js
@@ -9,6 +9,7 @@ const createNote = async (page, content) => {
   await page.getByRole('button', { name: 'new note' }).click()
   await page.getByTestId('note-content').fill(content)
   await page.getByRole('button', { name: 'save' }).click()
+  await page.getByText(content).waitFor()
 }
 
 export { loginWith, createNote }
--- e2e-playwright/tests/note_app.spec.js
+++ e2e-playwright/tests/note_app.spec.js
@@ -61,12 +61,20 @@ describe('Note app', () => {
 
     describe('and a note exists', () => {
       beforeEach(async ({ page }) => {
-        await createNote(page, 'another note by playwright')
+        await createNote(page, 'first note', true)
+        await createNote(page, 'second note', true)
+        await createNote(page, 'third note', true)
       })
 
       test('importance can be changed', async ({ page }) => {
-        await page.getByRole('button', { name: 'make not important' }).click()
-        await expect(page.getByText('make important')).toBeVisible()
+        await page.pause()
+        const otherNoteText = page.getByText('second note')
+        const otherNoteElement = otherNoteText.locator('..')
+
+        await otherNoteElement
+          .getByRole('button', { name: 'make not important' })
+          .click()
+        await expect(otherNoteElement.getByText('make important')).toBeVisible()
       })
     })
   })
