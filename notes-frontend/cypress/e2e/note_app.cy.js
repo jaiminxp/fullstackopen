45f2251a99b13a0d0b1ff609f125fa760de8cc8a
Jaimin Parmar <jaimins100@gmail.com>
2024-08-03 11:30:27 +0530

    add test for failed login

--- notes-frontend/cypress/e2e/note_app.cy.js
+++ notes-frontend/cypress/e2e/note_app.cy.js
@@ -28,6 +28,22 @@ describe('Note app', function () {
     cy.contains('Jaimin logged-in')
   })
 
+  it('login fails with wrong password', function () {
+    cy.contains('login').click()
+
+    cy.get('#username').type('admin')
+    cy.get('#password').type('wrong')
+
+    cy.get('#login-button').click()
+
+    // cy.get('.error').contains('Wrong credentials')
+    cy.get('.error').should('contain', 'Wrong credentials')
+    cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    cy.get('.error').should('have.css', 'border-style', 'solid')
+
+    cy.get('html').should('not.contain', 'Jaimin logged-in')
+  })
+
   describe('when logged in', function () {
     beforeEach(function () {
       cy.contains('login').click()
