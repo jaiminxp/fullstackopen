6d65cedd4dbcad114eae6082f3238e02a879833b
Jaimin Parmar <jaimins100@gmail.com>
2024-08-07 22:53:43 +0530

    combine filter and note reducers

--- redux-demo/src/App.jsx
+++ redux-demo/src/App.jsx
@@ -2,9 +2,35 @@ import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 
 function App() {
+  const filterSelected = (value) => {
+    console.log(value)
+  }
+
   return (
     <div>
       <NewNote />
+
+      <div>
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('ALL')}
+        />
+        all
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('IMPORTANT')}
+        />
+        important
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('NONIMPORTANT')}
+        />
+        nonimportant
+      </div>
+
       <Notes />
     </div>
   )
--- redux-demo/src/main.jsx
+++ redux-demo/src/main.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import noteReducer from '@reducers/noteReducer'
+import { createStore, combineReducers } from 'redux'
+import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 import App from './App'
 import { Provider } from 'react-redux'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+})
+
+const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <div />
   </Provider>
 )
