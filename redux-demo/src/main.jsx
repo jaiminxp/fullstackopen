89868fd47d99e3bdf4d58708c3e0ede875975dca
Jaimin Parmar <jaimins100@gmail.com>
2024-08-07 22:27:28 +0530

    assign initial state to noteReducer

--- redux-demo/src/main.jsx
+++ redux-demo/src/main.jsx
@@ -7,24 +7,6 @@ import { Provider } from 'react-redux'
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1,
-  },
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2,
-  },
-})
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
