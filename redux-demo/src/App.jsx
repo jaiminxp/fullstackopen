ddc5e992e0531f2a7f007bc5fdb97540ba87fe63
Jaimin Parmar <jaimins100@gmail.com>
2024-08-08 11:05:37 +0530

    fetching notes from the backend

--- redux-demo/src/App.jsx
+++ redux-demo/src/App.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from 'react'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
+import { useDispatch } from 'react-redux'
+import noteService from './services/notes'
+import { setNotes } from './reducers/noteReducer'
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    noteService.getAll().then((notes) => {
+      dispatch(setNotes(notes))
+    })
+  }, [])
+
   return (
     <div>
       <NewNote />
