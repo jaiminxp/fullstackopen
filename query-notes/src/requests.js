081ac8cda328a0bc94e5e007634322ac55fcb523
Jaimin Parmar <jaimins100@gmail.com>
2024-08-11 20:43:17 +0530

    update note mutation

--- query-notes/src/requests.js
+++ query-notes/src/requests.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
+const baseUrl = 'http://localhost:3001'
+
 export const getNotes = () =>
-  axios.get('http://localhost:3001/notes').then((res) => res.data)
+  axios.get(`${baseUrl}/notes`).then((res) => res.data)
 
 export const createNote = (newNote) =>
-  axios.post('http://localhost:3001/notes', newNote).then((res) => res.data)
+  axios.post(`${baseUrl}/notes`, newNote).then((res) => res.data)
+
+export const updateNote = (updatedNote) =>
+  axios
+    .put(`${baseUrl}/notes/${updatedNote.id}`, updatedNote)
+    .then((res) => res.data)
