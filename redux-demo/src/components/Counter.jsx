a37dab6919e6b09cdc935b49c099df418cbd71b5
Jaimin Parmar <jaimins100@gmail.com>
2024-08-11 21:19:19 +0530

    Manage state using context

--- redux-demo/src/components/Counter.jsx
+++ redux-demo/src/components/Counter.jsx
@@ -1,28 +1,23 @@
-import { useReducer } from 'react'
+import { useCounterValue, useCounterDispatch } from '../context/CounterContext'
 
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1
-    case 'DEC':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
+const Display = () => {
+  const counter = useCounterValue()
+  return <div>{counter}</div>
+}
+
+const Button = ({ type, label }) => {
+  const dispatch = useCounterDispatch()
+  return <button onClick={() => dispatch({ type })}>{label}</button>
 }
 
 const Counter = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
-
   return (
     <div>
-      <div>{counter}</div>
+      <Display />
       <div>
-        <button onClick={() => counterDispatch({ type: 'INC' })}>+</button>
-        <button onClick={() => counterDispatch({ type: 'DEC' })}>-</button>
-        <button onClick={() => counterDispatch({ type: 'ZERO' })}>0</button>
+        <Button type="INC" label="+" />
+        <Button type="DEC" label="-" />
+        <Button type="ZERO" label="0" />
       </div>
     </div>
   )
--- redux-demo/src/main.jsx
+++ redux-demo/src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
+import Counter from './components/Counter'
+import { CounterContextProvider } from './context/CounterContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
+    {/* <CounterContextProvider>
+      <Counter />
+    </CounterContextProvider> */}
   </Provider>
 )
