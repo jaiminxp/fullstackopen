da423e6f19a380f86a212d6c3cf7a94dda9e0fda
Jaimin Parmar <jaimins100@gmail.com>
2024-08-15 21:09:55 +0530

    use bootstrap form components

--- misc/src/components/RouterDemoBootstrap.jsx
+++ misc/src/components/RouterDemoBootstrap.jsx
@@ -8,7 +8,7 @@ import {
 } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { useState } from 'react'
-import { Table } from 'react-bootstrap'
+import { Table, Form, Button } from 'react-bootstrap'
 
 const Notes = ({ notes }) => {
   return (
@@ -83,15 +83,21 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username: <input />
-        </div>
-        <div>
-          password: <input type="password" />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <Form.Control type="text" name="username" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>password:</Form.Label>
+          <Form.Control type="password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          login
+        </Button>
+      </Form>
     </div>
   )
 }
