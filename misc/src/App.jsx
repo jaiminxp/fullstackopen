86d62cdedef68a9ab44b9d431a75e14ad8f3a8c4
Jaimin Parmar <jaimins100@gmail.com>
2024-08-16 15:26:42 +0530

    react class component

--- misc/src/App.jsx
+++ misc/src/App.jsx
@@ -4,14 +4,16 @@ import CustomHookDemo from './components/CustomHookDemo'
 import RouterDemoBootstrap from './components/RouterDemoBootstrap'
 import RouterDemoMUI from './components/RouterDemoMUI'
 import RouterDemoStyled from './components/RouterDemoStyled'
+import ClassDemo from './components/ClassDemo'
 
 function App() {
   return (
     <>
+      <ClassDemo />
       {/* <CustomHookDemo /> */}
-      <Router>
+      {/* <Router>
         <RouterDemoStyled />
-      </Router>
+      </Router> */}
     </>
   )
 }
