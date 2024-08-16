3ea4978fb43b968d9779450ac10ee68b7b11fcb1
Jaimin Parmar <jaimins100@gmail.com>
2024-08-16 09:46:43 +0530

    install MUI

--- misc/src/App.jsx
+++ misc/src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import RouterDemo from './components/RouterDemo'
 import CustomHookDemo from './components/CustomHookDemo'
 import RouterDemoBootstrap from './components/RouterDemoBootstrap'
+import RouterDemoMUI from './components/RouterDemoMUI'
 
 function App() {
   return (
     <>
       {/* <CustomHookDemo /> */}
       <Router>
-        <RouterDemoBootstrap />
+        <RouterDemoMUI />
       </Router>
     </>
   )
