4e076a09c4e7b19e8817ce51a489171d5f81021f
Jaimin Parmar <jaimins100@gmail.com>
2024-08-16 12:22:06 +0530

    include css in build

--- webpack/src/App.js
+++ webpack/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react' // we need this now also in component files
 
 const App = () => {
-  return <div>hello webpack</div>
+  return <div className="container">hello webpack</div>
 }
 
 export default App
